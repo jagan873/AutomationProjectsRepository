6215f0d0cf9bba8968b309af0e65f2f5df7bd0f6
jaganmohan <prjagan@gmail.com>
2025-03-05 17:12:04 +0530

    adding new repo
    adding new repo

---
+++ testautomation/src/main/java/com/tataelxsi/automationprojects/TestautomationApplication.java
@@ -0,0 +1,13 @@
+package com.tataelxsi.automationprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestautomationApplication.class, args);
+	}
+
+}
---
+++ testautomation/src/test/java/com/tataelxsi/automationprojects/TestautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tataelxsi.automationprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
